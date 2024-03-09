a5c83b5244dc51b5561c3f5a004feeb6c483cd9b
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-03-09 13:15:42 -0600

    add drawer navigation

--- screens/MainComponent.jsx
+++ screens/MainComponent.jsx
@@ -5,19 +5,34 @@ import Constants from "expo-constants";
 import DirectoryScreen from "./DirectoryScreen";
 import CampsiteInfoScreen from "./CampsiteInfoScreen";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import HomeScreen from "./HomeScreen";
+
+const Drawer = createDrawerNavigator();
+
+const screenOptions = {
+	headerStyle: {
+		backgroundColor: "#5637DD",
+	},
+	headerTintColor: "#fff",
+};
+const HomeNavigator = () => {
+	const Stack = createStackNavigator();
+	return (
+		<Stack.Navigator screenOptions={screenOptions}>
+			<Stack.Screen
+				name="Home"
+				component={HomeScreen}
+				options={{ title: "Home" }}
+			></Stack.Screen>
+		</Stack.Navigator>
+	);
+};
 
 const DirectoryNavigator = () => {
 	const Stack = createStackNavigator();
 	return (
-		<Stack.Navigator
-			initialRouteName="Directory"
-			screenOptions={{
-				headerStyle: {
-					backgroundColor: "#5637DD",
-				},
-				headerTintColor: "#fff",
-			}}
-		>
+		<Stack.Navigator initialRouteName="Directory" screenOptions={screenOptions}>
 			<Stack.Screen
 				name="Directory"
 				component={DirectoryScreen}
@@ -56,7 +71,22 @@ const Main = () => {
 					campsites.filter((campsite) => campsite.id === selectedCampsiteId)[0]
 				}
 			></CampsiteInfoScreen> */}
-			<DirectoryNavigator />
+			<Drawer.Navigator
+				initialRouteName="Home"
+				drawerStyle={{ backgroundColor: "#CEC8FF" }}
+			>
+				<Drawer.Screen
+					name="Home"
+					component={HomeNavigator}
+					options={{ title: "Home" }}
+				/>
+
+				<Drawer.Screen
+					name="Directory"
+					component={DirectoryNavigator}
+					options={{ title: "Directory" }}
+				/>
+			</Drawer.Navigator>
 		</View>
 	);
 };
