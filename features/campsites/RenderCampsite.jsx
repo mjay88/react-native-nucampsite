12b96f0cdd131c925d538aa862679d340b03df58
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-03-22 15:15:21 -0500

    add rubberBand gesture to RenderCampsite.jsx

--- features/campsites/RenderCampsite.jsx
+++ features/campsites/RenderCampsite.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { Text, View, StyleSheet, PanResponder, Alert } from "react-native";
 import { Card, Icon } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
@@ -6,11 +7,19 @@ import * as Animatable from "react-native-animatable";
 import React from "react";
 
 const RenderCampsite = (props) => {
+	const view = useRef();
 	const { campsite } = props;
 
 	const isLeftSwipe = ({ dx }) => dx < -200;
 	const panResponder = PanResponder.create({
 		onStartShouldSetPanResponder: () => true,
+		onPanResponderGrant: () => {
+			view.current
+				.rubberBand(1000)
+				.then((endState) =>
+					console.log(endState.finished ? "finished" : "canceled")
+				);
+		},
 		onPanResponderEnd: (e, gestureState) => {
 			console.log("pan responder end", gestureState);
 			if (isLeftSwipe(gestureState)) {
@@ -43,6 +52,7 @@ const RenderCampsite = (props) => {
 				animation="fadeInDownBig"
 				duration={2000}
 				delay={1000}
+				ref={view}
 				{...panResponder.panHandlers}
 			>
 				<Card containerStyle={styles.cardContainer}>
